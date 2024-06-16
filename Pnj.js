5a30af508fa8ae0a61d3e71a172feedb7d8f6171
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2024-06-16 22:28:57 +0200

    :zap: affichage donnee

---
+++ Pnj.js
@@ -0,0 +1,26 @@
+
+<div className="col-md-4">
+    <div className="mb-3 pb-2">
+        <div className="date-wrapper">
+            <div className="post"
+                 style="background-image:url('https://cdn.discordapp.com/attachments/1251252569691656204/1251252628776685758/image.png?ex=666de751&is=666c95d1&hm=263d50b78682c14dc35f05a5d5f0bc6394c50f6a706ce2b28e291bd9d6ec007b&');">
+                <div className="content">
+                    <h5 className="title mt-2">
+                        Pousse de Bambou
+                    </h5>
+                    <p className="description mb-1">
+                        Pnj : Kazuo / Ville : Shinshiro
+                        <br>
+                            position : x=-14142,y=73,z=548
+                    </p>
+                    <div className="action">
+                        <span className="value">5 blocs d'émeraude</span>
+                    </div>
+                </div>
+            </div>
+            <h6 className="date">
+                <span className="value">peux etre acheté</span>
+            </h6>
+        </div>
+    </div>
+</div>
