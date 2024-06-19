a866f74fea8474df5f9e5d84127f42462d99b45f
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2024-06-19 21:18:27 +0200

    :zap: lampion shinshiro ( manques les images)

--- Pnj.js
+++ Pnj.js
@@ -1,26 +1,5 @@
-
-<div className="col-md-4">
-    <div className="mb-3 pb-2">
-        <div className="date-wrapper">
-            <div className="post"
-                 style="background-image:url('https://cdn.discordapp.com/attachments/1251252569691656204/1251252628776685758/image.png?ex=666de751&is=666c95d1&hm=263d50b78682c14dc35f05a5d5f0bc6394c50f6a706ce2b28e291bd9d6ec007b&');">
-                <div className="content">
-                    <h5 className="title mt-2">
-                        Pousse de Bambou
-                    </h5>
-                    <p className="description mb-1">
-                        Pnj : Kazuo / Ville : Shinshiro
-                        <br>
-                            position : x=-14142,y=73,z=548
-                    </p>
-                    <div className="action">
-                        <span className="value">5 blocs d'émeraude</span>
-                    </div>
-                </div>
-            </div>
-            <h6 className="date">
-                <span className="value">peux etre acheté</span>
-            </h6>
-        </div>
-    </div>
-</div>
+decos.push(createDeco("Lampion   jaune   simple", getPnj(11),"64 teintures   jaune  s / 1 Almericoins bronze ","","oui",["lampion"]));
+decos.push(createDeco("Lampion   jaune   à frange", getPnj(11),"Lampion   jaune   simple / 16 pepites d'or","","oui",["lampion"]));
+decos.push(createDeco("Lampion   jaune   colonne", getPnj(11),"3 Lampion   jaune   simple / 32 papier","","oui",["lampion"]));
+decos.push(createDeco("Mini-Lampion   jaune   double droit", getPnj(11),"1 Lampion   jaune   simple / 1 baton","","oui",["lampion"]));
+decos.push(createDeco("Mini-Lampion   jaune   double de coin", getPnj(11),"1 Lampion   jaune   simple / 1 baton","","oui",["lampion"]));
