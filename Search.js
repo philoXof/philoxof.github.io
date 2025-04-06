5757a0f9fed2c8d3a1fd8382820e52385b4657f8
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2025-04-06 21:53:27 +0200

    :zap: clean code réorganisation fonctionelle (tjrs pb avec recherche via ville)

---
+++ Search.js
@@ -0,0 +1,141 @@
+class Search{
+    levenshteinDistance = (s, t) => {
+        if (!s.length) return t.length;
+        if (!t.length) return s.length;
+        const arr = [];
+        for (let i = 0; i <= t.length; i++) {
+            arr[i] = [i];
+            for (let j = 1; j <= s.length; j++) {
+                arr[i][j] =
+                    i === 0
+                        ? j
+                        : Math.min(
+                            arr[i - 1][j] + 1,
+                            arr[i][j - 1] + 1,
+                            arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
+                        );
+            }
+        }
+        return arr[t.length][s.length];
+    };
+    calculate = (a,b) => {
+        return levenshteinDistance(a.toLowerCase(),b.toLowerCase()) < 3;
+    }
+
+    getPnjByName = (name) =>{
+        let result = [];
+        pnjs.forEach(pnj => {
+            if (calculate(name,pnj._name)){
+                if (!result.includes(pnj)){
+                    result.push(pnj);
+                }
+            }
+        });
+        return result;
+    }
+    getDecoInPnj = (pnj)=>{
+        let result = [];
+        decos.forEach(deco=>{
+            if(deco.pnj_id === pnj){
+                result.push(deco);
+            }
+        });
+        return result;
+    }
+
+    /**
+     *
+     * @param recherche = contenue de l'entrée utilisateur
+     * @returns {*[deco(id, name, [pnj_id], image, price, event, craftable, [tags])]}
+     * ca retourne les decos, base sur le mot de recherche en condition qu il soit conforme avec le nom des maires ou de la ville
+     *
+     */
+    searchInVille = (recherche) => {
+        let result = [];
+        villes.forEach(city=> {
+
+            if (calculate(recherche,city._name)){
+                if(!result.includes(city)){
+                    result.push(city);
+                }
+            }
+
+            city._mayors.forEach(mayor => {
+                if (calculate(recherche,mayor)){
+                    if(!result.includes(city)){
+                        result.push(city);
+                    }
+                }
+            })
+
+        });
+
+        return result;
+    }
+
+    searchInTags = (recherche)=>{
+        const result = []
+        decos.forEach(deco => {
+            deco.tags.forEach(tag=>{
+                if(calculate(recherche,tag))
+                    if(!result.includes(deco))
+                        result.push(deco);
+            });
+        });
+        return result;
+    }
+    searchDeco = (recherche) =>{
+        let result = [];
+        const splitSearch = recherche.split(" ");
+
+        splitSearch.forEach(word=>{
+
+            decos.forEach(deco =>{
+                if(calculate(word,deco._name)){
+                    if(!result.includes(deco))
+                        result.push(deco);
+                }
+            });
+
+            searchInVille(word).forEach(ville=>{
+                getDecoInVille(ville,decos).forEach(deco => {
+                    if(!result.includes(deco))
+                        result.push(deco);
+                });
+            });
+
+            getPnjByName(word).forEach(pnj => {
+                getDecoInPnj(pnj).forEach(deco => {
+                    if(!result.includes(deco))
+                        result.push(deco);
+                });
+            });
+
+            searchInTags(word).forEach(deco=>{
+                if(!result.includes(deco))
+                    result.push(deco);
+            });
+
+        });
+        return result;
+    }
+    getVille = (name)=>{
+        let result;
+        villes.forEach(ville=> {
+            if(ville._name === name){
+                result = ville
+            }
+        });
+        return result;
+    }
+    getDecoInVille = (ville, decos)=>{
+        let result = [];
+        decos.forEach(deco=>{
+            console.log(deco)
+            if(deco._pnj_id._ville_id._name === ville){
+                result.push(deco);
+            }
+        });
+        return result;
+    }
+}
--- Villes.js
+++ Villes.js
@@ -22,57 +22,64 @@ class Ville{
 }
 
 class Villes extends Ville{
+    villes = [];
     constructor() {
         super();
     }
 
-    InitCityData(){
-
-
-        const villes = [];
+    InitVilleData(){
         /** *
          * Déjà faite (decoration dedans)
          * **/
-        villes.push(new Ville(1,Eriador, ["jerem1664", "Nisudry"]));
-        villes.push(new Ville(2,Yrias, ["_Ybrakxar"]));
-        villes.push(new Ville(3,Shinshiro, ["DVN88819"]));
-        villes.push(new Ville(4,Drakaina,["_Minesta_"]));
-        villes.push(new Ville(5,Rome,["kiefer_grylls", "klein1445"]));
-        villes.push(new Ville(6,Florenia,["Flifloux"]));
-        villes.push(new Ville(7,Azuria,["Zappax0"]));
+        this.villes.push(new Ville(1,Eriador, ["jerem1664", "Nisudry"]));
+        this.villes.push(new Ville(2,Yrias, ["_Ybrakxar"]));
+        this.villes.push(new Ville(3,Shinshiro, ["DVN88819"]));
+        this.villes.push(new Ville(4,Drakaina,["_Minesta_"]));
+        this.villes.push(new Ville(5,Rome,["kiefer_grylls", "klein1445"]));
+        this.villes.push(new Ville(6,Florenia,["Flifloux"]));
+        this.villes.push(new Ville(7,Azuria,["Zappax0"]));
 
         /** *
          * En cours (dajout des decos)
          * **/
-        villes.push(new Ville(8,Nakaram,["Adidix"]));
+        this.villes.push(new Ville(8,Nakaram,["Adidix"]));
 
         /** *
          * A faire (données des deco)
          * **/
-        villes.push(new Ville(9,Villers,["Tintin1712"]));
-        villes.push(new Ville(10,Nassau,["Glommery"]));
-        villes.push(new Ville(11,Helios,["LeftForDeath6"]));
-        villes.push(new Ville(12,Inpou,["Gorderok", "Zzheyd"]));
-        villes.push(new Ville(13,Almeria,["Gorderok", "Zzheyd"]));
+        this.villes.push(new Ville(9,Villers,["Tintin1712"]));
+        this.villes.push(new Ville(10,Nassau,["Glommery"]));
+        this.villes.push(new Ville(11,Helios,["LeftForDeath6"]));
+        this.villes.push(new Ville(12,Inpou,["Gorderok", "Zzheyd"]));
+        this.villes.push(new Ville(13,Almeria,["Gorderok", "Zzheyd"]));
 
         /** *
          * Villes en construction
          * **/
-        villes.push(new Ville(14,Kotaiblis,["Xof__"]));
-        villes.push(new Ville(15,Imynda,["Djox79"]));
-        villes.push(new Ville(16,Highland,["MissDu39","Kimalo"]));
+        this.villes.push(new Ville(14,Kotaiblis,["Xof__"]));
+        this.villes.push(new Ville(15,Imynda,["Djox79"]));
+        this.villes.push(new Ville(16,Highland,["MissDu39","Kimalo"]));
 
         /** *
          * Villes Morte je crois xd
          * **/
-        villes.push(new Ville(17,Alorah,["Gorderok", "Zzheyd"]));
-        villes.push(new Ville(18,Blackbay,["Arcks21"]));
+        this.villes.push(new Ville(17,Alorah,["Gorderok", "Zzheyd"]));
+        this.villes.push(new Ville(18,Blackbay,["Arcks21"]));
 
-        villes.push(new Ville(19,Pas2Ville,["Pas2Maire"]));
+        this.villes.push(new Ville(19,Pas2Ville,["Pas2Maire"]));
 
-        return villes;
+        return this.villes;
     }
 
+    getVilleByName(name){
+        let result;
+        this.villes.forEach(ville=>{
+            if(ville._name.toString() === name){
+                result = ville;
+            }
+        });
+        return result;
+    }
 
 }
 
